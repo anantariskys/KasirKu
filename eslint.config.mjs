837325fd80cf04a83dbfaa77d753d87cb33609cd
anantariskys <gamersindo555@gmail.com>
2025-03-28 17:37:58 +0700

    Update ESLint configuration, add new dependencies, and enhance Tailwind CSS setup. Removed unused CSS file and refactored layout and page components for improved structure and styling.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**"
+    ]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
